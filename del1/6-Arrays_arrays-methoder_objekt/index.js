f2f75788261f19076045dc15be05d5286094930b
Caspar Danielsson <caspar.danielsson@iths.se>
2024-10-21 12:52:22 +0200

    add folder array-methods & objects. complete js file 6/7

---
+++ del1/6-Arrays_arrays-methoder_objekt/index.js
@@ -0,0 +1,60 @@
+// !Arrays
+//* Uppgift 1
+// let a = []
+
+//* Uppgift 2
+// let a = [1,2,3]
+
+//* Uppgift 3
+// let a = ["1", 2, true]
+
+//* Uppgift 4
+// let a = ["1", 1, 2, 2, 3, 3, ""]
+
+//* Uppgift 5 (VG)
+// let a = [2 ,10, 20]
+
+//! array-methoder & objekt
+
+//* Uppgift 1
+// let a = [5, 10]
+
+//* Uppgift 2
+/*
+let numbers = []
+
+for(let idx = 1; idx <= 13; idx++) {
+  numbers.push(idx)
+  }
+  */
+
+ //* Uppgift 3
+// Array.shift - deletes first index
+// Array.pop - deletes last index
+// Array.uhshift - adds to the start of the array
+
+// let a = [0, 5, -5, 0]
+
+//* Uppgift 4
+// let a = [1, 2]
+// let b = [3, 4, 5]
+
+//* Uppgift 5
+// let a = ["a", "aaa", "c"]
+// console.log(a[1].length === a.length + a[0].length -1)
+// console.log(`${a[1].length} === ${a.length} + ${a[0].length -1}`)
+// console.log(a[1].startsWith("a"))
+// console.log(a.indexOf("c")===2)
+// console.log(a[3])
+
+//! objekt
+
+//* Uppgift 1
+
+//* Uppgift 2
+
+//* Uppgift 3
+
+//* Uppgift 4
+
+//* Uppgift 5
---
+++ del1/7-arrays-methode/index.js
@@ -0,0 +1,44 @@
+//! array-methoder
+
+//* Uppgift 1
+// let a = [5, 10]
+
+//* Uppgift 2
+/*
+let numbers = []
+
+for(let idx = 1; idx <= 13; idx++) {
+  numbers.push(idx)
+  }
+  */
+
+ //* Uppgift 3
+// Array.shift - deletes first index
+// Array.pop - deletes last index
+// Array.uhshift - adds to the start of the array
+
+// let a = [0, 5, -5, 0]
+
+//* Uppgift 4
+// let a = [1, 2]
+// let b = [3, 4, 5]
+
+//* Uppgift 5
+// let a = ["a", "aaa", "c"]
+// console.log(a[1].length === a.length + a[0].length -1)
+// console.log(`${a[1].length} === ${a.length} + ${a[0].length -1}`)
+// console.log(a[1].startsWith("a"))
+// console.log(a.indexOf("c")===2)
+// console.log(a[3])
+
+//! objekt
+
+//* Uppgift 1
+
+//* Uppgift 2
+
+//* Uppgift 3
+
+//* Uppgift 4
+
+//* Uppgift 5
